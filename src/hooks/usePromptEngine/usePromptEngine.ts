3cdf2b3959441cef9fdf004cb7f46a8095c49599
BillyLuisNeedham <billyluisneedham@duck.com>
2023-09-03 16:50:36 +0100

    refactor: break usePromptEngine into smaller functions

--- src/hooks/usePromptEngine/usePromptEngine.ts
+++ src/hooks/usePromptEngine/usePromptEngine.ts
@@ -2,13 +2,7 @@ import { PromptEngine } from "../../functions/PromptEngine/PromptEngine";
 import { useState } from "react";
 
 export function usePromptEngine(questions: Question[]) {
-  const initialPromptState: PromptInProgress = {
-    questions,
-    answers: [],
-    status: "in progress",
-  };
-
-  const [value, setValue] = useState<PromptState>(initialPromptState);
+  const [value, setValue] = useState<PromptState>(getInitialState(questions));
 
   function handleChange(answer: string) {
     setValue(PromptEngine(value, answer));
@@ -17,6 +11,14 @@ export function usePromptEngine(questions: Question[]) {
   return {
     value,
     onNewAnswer: handleChange,
+  };
+}
+
+function getInitialState(questions: Question[]): PromptInProgress {
+  return {
+    questions,
+    answers: [],
+    status: "in progress",
   };
 }
 

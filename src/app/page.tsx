b80d949fc670955d53286a412ba000b0402b0da9
BillyLuisNeedham <billyluisneedham@duck.com>
2023-09-03 17:18:17 +0100

    wip: build out page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,44 @@ import { useState } from "react";
 import { Button } from "../components/Button";
 import { TextBox } from "../components/TextBox";
 import { TextInput } from "../components/TextInput";
+import usePromptEngine from "@/hooks/usePromptEngine/usePromptEngine";
+import { guideQuestions } from "@/data/guideQuestions";
 
 export default function Home() {
-  const [text, setText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const promptState = usePromptEngine(guideQuestions);
+  const [displayText, setDisplayText] = useState(guideQuestions[0].question);
 
   const handleButtonClick = () => {
-    console.log(text);
+    promptState.onNewAnswer(inputText);
+    setInputText("");
+    if (promptState.value.status === "in progress") {
+      const nextQuestion =
+        promptState.value.questions[promptState.value.answers.length];
+      const lastAnswer =
+        promptState.value.answers[promptState.value.answers.length - 1];
+
+      setDisplayText(
+        `${displayText}
+        
+        ${lastAnswer}
+        
+        ${nextQuestion.question}`
+      );
+    }
+
+    if (promptState.value.status === "complete") {
+      setDisplayText(promptState.value.prompt);
+    }
   };
 
   return (
     <div className="p-4">
-      <TextBox>This is the box of text.</TextBox>
+      <TextBox>{displayText}</TextBox>
       <div className="flex mt-4">
         <TextInput
-          value={text}
-          onChange={setText}
+          value={inputText}
+          onChange={setInputText}
           placeholder="Enter text here"
         />
         <Button onClick={handleButtonClick}>Click me</Button>

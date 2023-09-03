174fd592f86b92d5753f5f20dc5a9b58f217f5e3
BillyLuisNeedham <billyluisneedham@duck.com>
2023-09-03 17:00:43 +0100

    feat: add guideQuestions

---
+++ src/data/guideQuestions.ts
@@ -0,0 +1,22 @@
+export const guideQuestions: Question[] = [
+    {
+        question: "Goal: What do you want to accomplish? Example: Make an app to help kids with their homework",
+        answerPrefix: "Goal: ",
+    },
+    {
+        question: "User: Who is your user? Example: 15 year old children in school",
+        answerPrefix: "User: ",
+    },
+    {
+        question: "Instructions: What do you want your app to do? Example: The app should let kids type in questions and get answers",
+        answerPrefix: "Instructions: ",
+    },
+    {
+        question: "Detail: What are the details of your app? Example: The app should have a text box for kids to type in questions, and a button to submit the question",
+        answerPrefix: "Detail: ",
+    },
+    {
+        question: "Example: What is an example of your app? Example: Think about games you play that teach you things",
+        answerPrefix: "Example: ",
+    },
+];

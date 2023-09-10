6a88fc59952b09d9680001d0186dbdacfeceb4f0
BillyLuisNeedham <billyluisneedham@duck.com>
2023-09-10 21:25:37 +0100

    feat: text input multi line

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 
 interface TextInputProps {
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
-  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLTextAreaElement>) => void;
 }
 
 export const TextInput: React.FC<TextInputProps> = ({
@@ -13,14 +13,35 @@ export const TextInput: React.FC<TextInputProps> = ({
   placeholder,
   onKeyDown,
 }) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    }
+    onChange(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+    if (onKeyDown) {
+      onKeyDown(event);
+    }
+  };
+
   return (
     <textarea
-      className="bg-transparent flex-1 focus:outline-none w-full"
-      type="text"
+      ref={textareaRef}
+      className="bg-transparent flex-1 focus:outline-none w-full resize-none"
+      rows={1}
       value={value}
-      onChange={(e) => onChange(e.target.value)}
+      onChange={handleChange}
       placeholder={placeholder}
-      onKeyDown={onKeyDown}
+      onKeyDown={handleKeyDown}
     />
   );
 };

8c595007c8932f1d8e519cb76b512eea4025b60b
BillyLuisNeedham <billyluisneedham@duck.com>
2023-09-12 20:49:11 +0100

    feat: add additional testing for DisplayOptions

--- src/functions/DisplayOptions/DisplayOptions.test.ts
+++ src/functions/DisplayOptions/DisplayOptions.test.ts
@@ -1,25 +1,66 @@
+import { guideOption } from "../../data/PromptOptions/guideQuestions";
 import { displayOptions } from "./DisplayOptions";
+import { swissOption } from "../../data/PromptOptions/swissQuestions";
 
-describe('displayOptions', () => {
+describe("displayOptions", () => {
+  // Test that the function 'displayOptions' returns an object with status "in progress" and the options array when the command is null.
+  it('should return an object with status "in progress" and the options array when the command is null', () => {
+    // Arrange
+    const options: PromptOption[] = [guideOption, swissOption];
+    const command = null;
 
-    // Test that the function 'displayOptions' returns an object with status "in progress" and the options array when the command is null.
-    it('should return an object with status "in progress" and the options array when the command is null', () => {
+    // Act
+    const result = displayOptions(options, command);
+
+    // Assert
+    expect(result).toEqual({
+      status: "in progress",
+      options: options,
+    });
+  });
+
+  // Test that the function 'displayOptions' returns an object with status "complete" and the selected questions array when a valid command is passed.
+  it('should return an object with status "complete" and the selected questions array when a valid command is passed', () => {
+    const testRounds = [
+      {
+        command: guideOption.commandCode,
+        expected: guideOption.questions,
+      },
+      {
+        command: swissOption.commandCode,
+        expected: swissOption.questions,
+      },
+    ];
+
+    testRounds.forEach((round) => {
       // Arrange
-      const options: PromptOption[] = [
-        { commandCode: "A", questions: ["Question 1", "Question 2"] },
-        { commandCode: "B", questions: ["Question 3", "Question 4"] },
-      ];
-      const command = null;
+      const options: PromptOption[] = [guideOption, swissOption];
+      const command = round.command;
 
       // Act
       const result = displayOptions(options, command);
 
       // Assert
       expect(result).toEqual({
-        status: "in progress",
-        options: options,
+        status: "complete",
+        selectedQuestions: round.expected,
       });
     });
+  });
 
+  // Test that the function 'displayOptions' returns an object with status "in progress" and the options array when an invalid command is passed.
+  it('should return an object with status "in progress" and the options array when an invalid command is passed', () => {
+    // Arrange
+    const options: PromptOption[] = [guideOption, swissOption];
+    const command = "invalidCommand";
 
+    // Act
+    const result = displayOptions(options, command);
+
+    // Assert
+    expect(result).toEqual({
+      status: "in progress",
+      options: options,
+    });
+  });
 });

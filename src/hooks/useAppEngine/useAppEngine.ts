14c2ac7060ec9d306c605d4f6154a14c2fac462c
BillyLuisNeedham <billyluisneedham@duck.com>
2023-09-13 21:15:32 +0100

    feat: build useAppEngine

---
+++ src/hooks/useAppEngine/useAppEngine.ts
@@ -0,0 +1,29 @@
+import { appEngine } from "../../functions/AppEngine/AppEngine";
+import { useState } from "react";
+
+export function useAppEngine(promptOptions: PromptOption[]) {
+  const [value, setValue] = useState<AppEngineState>(
+    appEngine(getInitialState(promptOptions), null)
+  );
+
+  function handleChange(command: string) {
+    setValue(appEngine(value, command));
+  }
+
+  function reset() {
+    setValue(appEngine(getInitialState(promptOptions), null));
+  }
+
+  return {
+    value,
+    onNewCommand: handleChange,
+    reset,
+  };
+}
+
+function getInitialState(promptOptions: PromptOption[]): AppEngineInitialState {
+  return {
+    status: "initial",
+    options: promptOptions,
+  };
+}

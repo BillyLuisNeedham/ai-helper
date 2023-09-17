9883a02a2525ecaacd06b6d8cfaa53458951134c
BillyLuisNeedham <billyluisneedham@duck.com>
2023-09-17 20:30:04 +0100

    fix: broken import

--- src/functions/WelcomeMessage/WelcomeMessage.ts
+++ src/functions/WelcomeMessage/WelcomeMessage.ts
@@ -1,4 +1,4 @@
-import { welcomeMessages } from "@/data/welcomeMessages/welcomeMessages";
+import { welcomeMessages } from "../../data/welcomeMessages/welcomeMessages";
 
 export function getWelcomeMessage(messages: string[] = welcomeMessages): string {
     return messages[Math.floor(Math.random() * messages.length)];
